97957f807eca1d5fb069c64d1d6800f2e7166f23
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2022-01-17 17:28:53 +0530

    job

--- src/components/UserApply.jsx
+++ src/components/UserApply.jsx
@@ -1,11 +1,27 @@
 import { useSelector } from "react-redux"
-
+import './UserApply.css'
 export const UserApply=()=>{
 
     const {apply}=useSelector((state)=>({apply:state.job.apply}))
 console.log(apply)
 
     return <div>
+<h1>apply loginStatus</h1>
+<div id="outer">
+{
+            apply.map((el)=>(
+                <div id="inner">
+                    <p>company name {el.cname}</p>
+                    <p>job title {el.title}</p>
+                    <p>job salary {el.salary}</p>
+                    <p>job location {el.location}</p>
+                    <p>job discription {el.dis}</p>
+                  
+                </div>
+            ))
+        }
+</div>
+
 
     </div>
 }
